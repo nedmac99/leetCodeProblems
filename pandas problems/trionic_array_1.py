0f8ce6c56c778ffb94e06b3243adad7c3ed253cb
nedmac99 <camden.burdge@gmail.com>
2026-02-04 16:37:55 -0500

    fixed spelling errors, and started the structure to the solution

--- pandas problems/trionic_array_1.py
+++ pandas problems/trionic_array_1.py
@@ -1,5 +1,5 @@
 '''
-You are given an array nums of lenth n. An array is trionis if there exist indices 0 < p < q < n -1 such that:
+You are given an array nums of lenth n. An array is trionic if there exist indices 0 < p < q < n - 1 such that:
 nums[0...p] is strictly increasing,
 nums[p...q] is strictly decreasing,
 nums[q...n - 1] is strictly increasing.
@@ -14,12 +14,15 @@ Explanation:
 pick p = 2, q = 4
     * nums[0...2] = [1, 3, 5] is strictly increasing (1 < 3 < 5)
     * nums[2...4] = [5, 4, 2] is strictly descreasing (5 > 4 > 2)
-    * nums[4...5] = [2, 6] is strictly increasing (2, 6)
+    * nums[4...5] = [2, 6] is strictly increasing (2 < 6)
 
 Example 2:
 
 '''
 
-import pandas
-
-nums = pandas.array()
+class Solution(object):
+    def isTrionic(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
